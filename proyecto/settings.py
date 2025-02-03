6d717d289cae57ad1ed095928da8d11425c49bc0
trota12 <136854078+trota12@users.noreply.github.com>
2025-02-03 03:12:40 -0300

    Add files via upload

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -66,7 +67,7 @@ ROOT_URLCONF = 'proyecto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'aplicacion/templates')],  # Asegurar esta línea
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -152,3 +153,5 @@ WEBPAY_CONFIGURATION = {
     "api_key": "Tbk-Api-Key-Secret",
     "environment": "TEST"  # Para pruebas
 }
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
